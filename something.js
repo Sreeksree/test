a18af0b4ec667df60d051a1b83489625273795b0
Sreenath Kalarickal <kalarickalsreenath@gmail.com>
2021-10-06 20:29:32 +0530

    test commit

---
+++ something.js
@@ -0,0 +1,31 @@
+const a= 'test';
+let b = true;
+const c = 789;
+
+
+
+const person ={
+	firstName : "john",
+	lastName : "Doe",
+	age : 50,
+	eyeColor : "blue"
+};
+
+const {firstName,lastName,age,eyeColor} = person;
+
+const a ='test' ;
+let b =true;
+const c =789;
+
+const okObj = {
+	a,b,c
+};
+
+let messege =`hello ${firstName} have I met you before? I think we met in ${city} last summer no???`;
+
+function isValidAge(age=10) {
+	// body...
+	return age;
+}
+
+const s1=symbol("this is my first symbol")
